edd8753f068911eff0057f5b75b34d39d5ffc9a6
Sagar <sagarpuniyani@gmail.com>
2023-12-12 23:06:49 +0530

    Targets are Setted

---
+++ Recursion/hello.asm
@@ -0,0 +1,18 @@
+section .data
+    hello db 'Hello, World!',0
+
+section .text
+    global _start
+
+_start:
+    ; write the message to stdout
+    mov eax, 4          ; syscall: write
+    mov ebx, 1          ; file descriptor: stdout
+    mov ecx, hello      ; message to write
+    mov edx, 13         ; message length
+    int 0x80            ; call kernel
+
+    ; exit
+    mov eax, 1          ; syscall: exit
+    xor ebx, ebx        ; exit code
+    int 0x80            ; call kernel
